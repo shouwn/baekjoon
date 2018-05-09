6338a31f91b5b234cc3edaeded1b5c3465dd64c4
shouwn <sjonghyon@gmail.com>
2018-05-10 01:29:14 +0900

    수정 중

--- src/acmicpc/E3015.java
+++ src/acmicpc/E3015.java
@@ -1,53 +1,88 @@
 package acmicpc;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class E3015 {
 
-	private static final int NONE = -100;
+	public static void main(String[] args) throws IOException {
+		long time = System.currentTimeMillis();
+		try(BufferedReader reader = new BufferedReader(
+				new FileReader("F:\\Desktop\\test_data\\patrik\\patrik.in.8"))){
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+			int max = Integer.valueOf(reader.readLine());
 
-		int[] arr = new int[scan.nextInt()];
-		for(int i = 0; i < arr.length; i++)
-			arr[i] = scan.nextInt();
+			long count = 0;
+			MyStack stack = new MyStack();
+			stack.push(Integer.valueOf(reader.readLine()));
+			int index, input;
+			for(int i = 1; i < max; i++) {
+				input = Integer.valueOf(reader.readLine());
+				if(stack.peek() <= input) {
 
-		scan.close();
+					index = findIndexOfAbove(input, stack.arr, 0, stack.size() - 1);
+					count += stack.size() - index;
 
-		int count = 0;
-		
-		Stack<Integer> stack = new Stack<Integer>();
-		for(int i = 0; i < arr.length; i++) {
-			count 
+					while(!stack.isEmpty() && stack.peek() < input)
+						stack.pop();
+				}
+				else 
+					count++;
+
+				stack.push(input);
+			}
+			System.out.println(count);
 		}
+		System.out.println(System.currentTimeMillis() - time);
 	}
 
 	public static int findIndexOfAbove(int s, int[] a, int start, int end) {
 
-		if(start <= end) {
+		if(start < end) {
 			int middle = start + end;
 
-			if(a[middle] == s)
-				return middle;
-
-			if(a[middle] > s) {
-				int temp = findIndexOfAbove(s, a, middle + 1, end);
-				return  temp < 0 ? findIndexOfAbove(s, a, middle) : temp;
-			}
-			else {
-				int temp = findIndexOfAbove(s, a, start, middle - 1);
-				return temp < 0 ? findIndexOfAbove(s, a, middle) : temp;
-			}
+			if(a[middle] > s) 
+				return findIndexOfAbove(s, a, middle + 1, end);
+			else 
+				return findIndexOfAbove(s, a, start, middle - 1);
 		}
 		else
-			return NONE;
+			return findIndexOfAbove(s, a, start);
 	}
-	
-	public static int findIndexOfAbove(int s, int[] a, int start) {
-		while(a[start] > s)
-			start--;
-		return start;
+
+	public static int findIndexOfAbove(int s, int[] a, int index) {
+		for(; index > 0; index--) {
+			if(a[index] > s)
+				return index;
+		}
+
+		return index;
 	}
+}
+
+class MyStack{
+	int[] arr = new int[500001];
+	int rear;
+
+	public int pop() {
+		return arr[--rear];
+	}
+
+	public boolean isEmpty() {
+		return 0 == rear;
+	}
+
+	public void push(int item) {
+		arr[rear++] = item;
+	}
+
+	public int peek() {
+		return arr[rear-1];
+	}
+
+	public int size() {
+		return rear;
+	}
+
 }
