06f8ffb4b12a5701c9d406cb4e5576c63f94f5ce
shouwn <31586979+shouwn@users.noreply.github.com>
2018-05-16 18:18:32 +0900

    푸는 중

--- src/acmicpc/E1005.java
+++ src/acmicpc/E1005.java
@@ -3,7 +3,9 @@ package acmicpc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class E1005 {
@@ -31,10 +33,15 @@ public class E1005 {
 					int left = Integer.valueOf(input[0]);
 					int right = Integer.valueOf(input[1]);
 					
-					map.get(right).setMax(map.get(left));
+					map.get(left).next = map.get(right);
+					map.get(left).update();
 				}
 				
 				System.out.println("result " + map.get(Integer.valueOf(reader.readLine())));
+				System.out.println("3: " + map.get(3));
+				System.out.println("4: " + map.get(4));
+				System.out.println("5: " + map.get(5));
+				System.out.println("6: " + map.get(6));
 			}
 		}
 	}
@@ -43,24 +50,25 @@ public class E1005 {
 
 class MyNode {
 	int my;
-	int other;
-	MyNode next;
+	int from;
+	List<MyNode> nexts;
 	
 	public MyNode(int my) {
 		this.my = my;
+		nexts = new ArrayList<>();
 	}
 	
 	public int getTime() {
-		return my + other;
+		return my + from;
 	}
 	
-	public void setMax(MyNode other) {
-		if(this.other < other.getTime())
-			this.other = other.getTime();
-	}
-	
-	public void update() {
+	public static void update(MyNode current, MyNode next) {
 		
+		for(MyNode node : current.nexts)
+			if(current.getTime() > node.from)
+				next.from = current.getTime();
+		
+		next.update();
 	}
 	
 	@Override
