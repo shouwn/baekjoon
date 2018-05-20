796c49ba00b9062b63182c9442158ce4f427e989
shouwn <31586979+shouwn@users.noreply.github.com>
2018-05-21 08:53:30 +0900

    test commit

--- src/test/Test0_1.java
+++ src/test/Test0_1.java
@@ -6,13 +6,6 @@ public class Test0_1{
 	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) throws Exception{
-		
-		int sum = 0;
-		int i;
-		for(i = 1; sum + i < 10000; i++)
-			sum += i;
-		
-		System.out.println(sum + " " + i);
-		
 	}
 }
+
