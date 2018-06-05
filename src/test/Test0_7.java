c917724294cb3a5d281a9c9bd33c2afe18521f42
shouwn <31586979+shouwn@users.noreply.github.com>
2018-06-05 11:58:32 +0900

    test

--- src/test/Test0_7.java
+++ src/test/Test0_7.java
@@ -1,5 +1,32 @@
 package test;
 
 public class Test0_7 {
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new MyRunnable());
+		Thread t2 = new Thread(new MyRunnable());
+		Thread t3 = new Thread(new MyRunnable());
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		System.out.println("main end");
+	}
+	
+	static class MyRunnable implements Runnable{
 
+		@Override
+		public void run() {
+			for(int i = 0; i < 10; i++) {
+				System.out.println(Thread.currentThread().getName());
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
 }
