5536a09b7bb0289ebf0565bc8aa0040c0da8ef90
shouwn <sjonghyon@gmail.com>
2018-07-03 01:53:05 +0900

    15667번 품

---
+++ src/acmicpc/E15667.java
@@ -0,0 +1,23 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class E15667 {
+
+	public static void main(String[] args) {
+		try(Scanner scan = new Scanner(System.in)){
+			System.out.println(solution(scan.nextInt()));
+		}
+	}
+	
+	public static int solution(int N) {
+		int n = N - 1;
+		
+		for(int i = 1; i <= 100; i++) { //max 10101 - 1 = 100 * 101
+			if(i * (i + 1) == n)
+				return i;
+		}
+		
+		return 0;
+	}
+}
