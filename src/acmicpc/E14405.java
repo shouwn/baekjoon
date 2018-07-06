92c8c856adfe2d97e62f1792bf9cc89d7e57c571
shouwn <sjonghyon@gmail.com>
2018-07-07 01:50:00 +0900

    테스트 때문에 수정된 것

--- src/acmicpc/E14405.java
+++ src/acmicpc/E14405.java
@@ -9,6 +9,7 @@ public class E14405 {
 	public static void main(String[] args) {
 		try(Scanner scan = new Scanner(new BufferedReader(new InputStreamReader(System.in)))) {
 			System.out.println(solution(scan.nextLine()));
+			
 		}
 	}
 	
