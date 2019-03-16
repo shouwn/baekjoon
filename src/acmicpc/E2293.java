46903cdcd5ec168fd0479b7e7818885797942be9
shouwn <sjonghyon@gmail.com>
2019-03-16 09:00:04 +0900

    백준 2293 문제 해결

--- src/acmicpc/E2293.java
+++ src/acmicpc/E2293.java
@@ -1,86 +1,71 @@
 package acmicpc;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class E2293 {
 	
-	private static Map<Key, Integer> map = new HashMap<>();
-	private static int[] coins;
-	
-	public static void main(String[] args){
-		int type, goal;
-		Scanner scan = new Scanner(System.in);
-		
-		type = scan.nextInt();
-		goal = scan.nextInt();
-		
-		coins = new int[type];
-		for(int i = 0; i < type; i++){
-			coins[i] = scan.nextInt();
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			// index 0 is coin count, index 1 is value
+			int[] metaData = Arrays.stream(br.readLine().split(" "))
+					.mapToInt(Integer::parseInt)
+					.toArray();
+
+			int[] coins = Stream.iterate(0, n -> n + 1).limit(metaData[0])
+					.map(n -> {
+						try {
+							return br.readLine();
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+					})
+					.mapToInt(Integer::parseInt)
+					.toArray();
+
+			System.out.println(solution2(coins, metaData[1]));
 		}
-		
-		System.out.println(getCases(type - 1, goal));
-		
 	}
-	
-	public static int getCases(int capacity, int goal){
-		Key key = new Key(capacity, goal);
-		
-		if(map.containsKey(key))
-			return map.get(key);
-		
-		if(capacity < 0)
-			return 0;
-		
-		if(goal == 0)
+
+	public static int solution1(int[] coins, int value) {
+		return solution1(coins, value, 0);
+	}
+
+	public static int solution1(int[] coins, int value, int index) {
+		if (value == 0) {
 			return 1;
-		if(goal < 0)
+		} else if (index >= coins.length || value < 0) {
 			return 0;
-		
-		int sum = getCases(capacity - 1, goal) + getCases(capacity, goal - coins[capacity]);
-		
-		map.put(key, sum);
-		
+		}
+
+		int sum = 0;
+
+		for (int i = 0, maxCoinCount = value / coins[index]; i <= maxCoinCount; i++) {
+			sum += solution1(coins, value - i * coins[index], index + 1);
+		}
+
 		return sum;
 	}
-	
-	static class Key{
-		int capacity;
-		int goal;
-		
-		public Key(int capacity, int goal) {
-			super();
-			this.capacity = capacity;
-			this.goal = goal;
+
+	public static int solution2(int[] coins, int value) {
+
+		int[] memo = new int[value + 1];
+		memo[0] = 1;
+
+		for (int i = 0, coinsCount = coins.length; i < coinsCount; i++) {
+
+			int coin = coins[i];
+
+			for (int k = 1; k <= value; k++) {
+				if (k - coin >= 0) {
+					memo[k] += memo[k - coin];
+				}
+			}
 		}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + capacity;
-			result = prime * result + goal;
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Key other = (Key) obj;
-			if (capacity != other.capacity)
-				return false;
-			if (goal != other.goal)
-				return false;
-			return true;
-		}
-		
+		return memo[value];
 	}
-	
 }
